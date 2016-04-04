1313cad9a476bbf677fe77c6f41000f1d3c9892e
Pepe Maldonado <pepegvf@gmail.com>
2016-04-04 20:57:09 +0200

    CSS for menu

--- pr2/admin-index.php
+++ pr2/admin-index.php
@@ -3,12 +3,11 @@
   <head>
     <meta charset="UTF-8">
     <title>Internprise - Portal Administracion</title>
-  <link rel="stylesheet" href="admin.css" type="text/css">
-   <link rel="stylesheet" href="menu.css" type="text/css">
+  <link rel="stylesheet" href="css/admin.css" type="text/css">
+   <link rel="stylesheet" href="css/menu.css" type="text/css">
     <link rel="stylesheet" href="header.css" type="text/css">
   </head>
   <body>
-  <h1> Internprise </h1>
   <div id="contenedor">
 	<?php include ('admin-menu.html'); ?>
 	<?php include ('admin-header.html'); ?>
@@ -16,42 +15,42 @@
 	<div id="buscador">
 	<input type="text" placeholder="Buscador de estudiantes /empresas">
 	</div>
-	
+
 	<div id="widgets">
 		<div class= "widget">
 			<div class="titulo">
-			
+
 			</div>
 			<div class="cuerpo">
-			
+
 			</div>
 		<div>
 		<div class= "widget">
 			<div class="titulo">
-			
+
 			</div>
 			<div class="cuerpo">
-			
+
 			</div>
 		<div>
 		<div class= "widget">
 			<div class="titulo">
-			
+
 			</div>
 			<div class="cuerpo">
-			
+
 			</div>
 		<div>
 		<div class= "widget">
 			<div class="titulo">
-			
+
 			</div>
 			<div class="cuerpo">
-			
+
 			</div>
 		<div>
 	</div>
-	
+
 	</div>
   </div>
   </body>
