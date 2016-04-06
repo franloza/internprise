4fcdc908b437cf61c059c6c17b5f25e5bc3061ca
hriesco <hectorriesco@hotmail.com>
2016-04-06 16:00:16 +0200

    Updated

--- pr2/admin-index.php
+++ pr2/admin-index.php
@@ -3,53 +3,17 @@
   <head>
     <meta charset="UTF-8">
     <title>Internprise - Portal Administracion</title>
-   <link rel="stylesheet" href="css/admin.css" type="text/css">
-   <link rel="stylesheet" href="css/menu.css" type="text/css">
+    <link rel="stylesheet" href="css/admin.css" type="text/css">
+    <link rel="stylesheet" href="css/menu.css" type="text/css">
     <link rel="stylesheet" href="css/header.css" type="text/css">
+    <link rel="stylesheet" href="css/contenido.css" type="text/css">
   </head>
   <body>
-  <h1> Internprise </h1>
-  <div id="container-index" class="admin-container">
-	<?php include ('admin-menu.html'); ?>
-	<?php include ('admin-header.html'); ?>
-	<div id="content-index" class="admin-content">
-        <div id="buscador">
-            <input type="text" placeholder="Buscador de estudiantes/empresas">
-        </div>
-        <div id="widgets">
-            <div class= "widget">
-                <div class="titulo">
-                
-                </div>
-                <div class="cuerpo">
-                
-                </div>
-            </div>
-            <div class= "widget">
-                <div class="titulo">
-                
-                </div>
-                <div class="cuerpo">
-                
-                </div>
-            </div>
-            <div class= "widget">
-                <div class="titulo">
-                
-                </div>
-                <div class="cuerpo">
-                
-                </div>
-            </div>
-            <div class= "widget">
-                <div class="titulo">
-                
-                </div>
-                <div class="cuerpo">
-                
-                </div>
-            </div>
-        </div>
+	<h1> Internprise </h1>
+	<div id="container-index" class="admin-container">
+		<?php include ('admin-menu.html'); ?>
+		<?php include ('admin-header.html'); ?>
+		<?php include ('admin-contenido.html'); ?>
 	</div>
   </body>
 </html>
