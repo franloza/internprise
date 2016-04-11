654c596b51ea0bee38cc565a0b06defad4f35486
Usuario Local <Usuario Local>
2016-04-11 12:38:44 +0200

    Visual improves

--- pr2/admin-dashboard.php
+++ pr2/admin-dashboard.php
@@ -6,15 +6,43 @@
         <?php include ('admin-menu.php'); ?>
         <?php include ('admin-titlebar.html'); ?>
         <div id="dashboard-content" class="admin-content"> 
-	       <div class="widget-container">
-		      <form class="form-search cf" action="">
-			     <input type="text" placeholder="Buscador de estudiantes / empresas">
-		      </form>
-	       </div>
+	        <div class="widget-container">
+                <form class="form-search cf" action="">
+                    <input type="text" placeholder="Buscador de estudiantes / empresas">
+                </form>
+	        </div>
             <div class="widget-container">
                 <div class="widget">
                     <header>
-                        <h3>Datos</h3>
+                        <h2>Nuevas ofertas</h2>
+                    </header>
+                    <div class="content">
+                        <ul>
+                            <li>Programador C++ en Coritel</li>
+                            <li>Administrador de redes en Telefonica</li>
+                            <li>Diseñador Web en Bankia</li>
+                            <li>Desarrollador de videojuegos en Activision</li>
+                        </ul>
+                    </div>
+                </div>    
+                <div class="widget">
+                    <header>
+                        <h2>Contratos activos</h2>
+                    </header>
+                    <div class="content">
+                        <ul>
+                            <li>Juan Machin en Red.es</li>
+                            <li>Francisco Gonzalez </li>
+                            <li>Linea 3</li>
+                            <li>Linea 4</li>
+                        </ul>
+                    </div>
+                </div>
+	        </div>
+            <div class="widget-container">
+                <div class="widget">
+                    <header>
+                        <h2>Nuevas demandas</h2>
                     </header>
                     <div class="content">
                         <ul>
@@ -24,49 +52,22 @@
                             <li>Linea 4</li>
                         </ul>
                     </div>
-                </div>    
-            <div class="widget">
-                <header>
-                    <h3>Datos</h3>
-                </header>
-                <div class="content">
-                    <ul>
-                        <li>Linea 1</li>
-                        <li>Linea 2</li>
-                        <li>Linea 3</li>
-                        <li>Linea 4</li>
-                    </ul>
                 </div>
-            </div>
-	   </div>
-        <div class="widget-container">
-            <div class="widget">
-                <header>
-                    <h3>Datos</h3>
-                </header>
-                <div class="content">
-                    <ul>
-                        <li>Linea 1</li>
-                        <li>Linea 2</li>
-                        <li>Linea 3</li>
-                        <li>Linea 4</li>
-                    </ul>
+                <div class="widget">
+                    <header>
+                        <h2>Dudas y sugerencias</h2>
+                    </header>
+                    <div class="content">
+                        <ul>
+                            <li>Linea 1</li>
+                            <li>Linea 2</li>
+                            <li>Linea 3</li>
+                            <li>Linea 4</li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div class="widget">
-                <header>
-                    <h3>Datos</h3>
-                </header>
-                <div class="content">
-                    <ul>
-                        <li>Linea 1</li>
-                        <li>Linea 2</li>
-                        <li>Linea 3</li>
-                        <li>Linea 4</li>
-                    </ul>
-                </div>
-            </div>
-	   </div>
+    	   </div>
+        </div>
     </div>
   </body>
 </html>
