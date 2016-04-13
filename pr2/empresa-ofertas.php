1fc4ac6f76ae906a31943fd9ef86e43ab017a7d9
alexmartin3 <alejandro.martin.rueda.92@gmail.com>
2016-04-13 23:52:29 +0200

    alex empresa-ofertas

--- pr2/empresa-ofertas.php
+++ pr2/empresa-ofertas.php
@@ -6,7 +6,58 @@
 	<?php include ('empresa-menu.html'); ?>
 	<?php include ('empresa-titlebar.html'); ?>
 	<div id="content-empresa-ofertas" class="content">
-		
+		<div class="crear">
+            <form action="empresa-crear-oferta.php">
+				<input type=submit value="Crear Oferta">
+			</form>
+        </div>
+	   <div id="tablas-ofertas">
+        <div id="tabla-ofertas" class="table-container">
+            <div class="table-header"> Ofertas disponibles </div>
+             <table class="admin-table">
+                <tr>
+                    <th>Empresa</th>
+                    <th>Puesto</th>
+                    <th>Sueldo</th>
+                    <th>Horas</th>
+                    <th>Plazas</th>
+                    <th>Accion</th>
+                </tr>
+                <tr>
+                    <td>Intel</td>
+                    <td>Especialista IT</td>
+                    <td>400€</td>
+                    <td>20</td>
+                    <td>2</td>
+                    <td><a href="empresa-ofertas-vista.php">Ver</a></td>
+                </tr>
+                <tr>
+                    <td>Google ESP</td>
+                    <td>Analista de datos</td>
+                    <td>500€</td>
+                    <td>25</td>
+                    <td>1</td>
+                    <td><a href="empresa-ofertas-vista.php">Ver</a></td>
+                </tr>
+                <tr>
+                    <td>Coritel</td>
+                    <td>Desarrollador Web</td>
+                    <td>450€</td>
+                    <td>25</td>
+                    <td>2</td>
+                    <td><a href="empresa-ofertas-vista.php">Ver</a></td>
+                </tr>
+                <tr>
+                    <td>Indra</td>
+                    <td>Seguridad y firewall</td>
+                    <td>400€</td>
+                    <td>20</td>
+                    <td>1</td>
+                    <td><a href="empresa-ofertas-vista.php">Ver</a></td>
+                </tr>
+            </table>
+        </div>
+	</div>
 	</div>
   <?php include ('empresa-footer.html'); ?>
   </div>
