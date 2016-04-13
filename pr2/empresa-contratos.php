c0fc667c7160b7e61e491781d5aea32b0c4f7751
alexmartin3 <alejandro.martin.rueda.92@gmail.com>
2016-04-14 00:36:28 +0200

    alex empresa todo

--- pr2/empresa-contratos.php
+++ pr2/empresa-contratos.php
@@ -6,7 +6,98 @@
 	<?php include ('empresa-menu.html'); ?>
 	<?php include ('empresa-titlebar.html'); ?>
 	<div id="content-empresa-contratos" class="content">
-		
+		<div id="tabla-historial" class="table-container">
+        <div class="table-header"> Contratos en vigor </div>
+        <table class="admin-table">
+        <tr>
+            <th>ID Contrato</th>
+            <th>Estudiante</th>
+            <th>Fecha Inicio</th>
+            <th>Finalización</th>
+            <th>Puesto</th>
+            <th>Sueldo</th>
+			<th>Ver</th>
+        </tr>
+        <tr>
+            <td>0051299</td>
+            <td>Antonio Perez Sanchez</td>
+            <td>12/03/2016</td>
+            <td>30/07/2016</td>
+            <td>Programador Java</td>
+			<td>400€</td>
+            <td><a href="admin-contratos-vista.php">Ver</a></td>
+        </tr>
+        <tr>
+            <td>0051100</td>
+            <td>Elvira Jurado Perez</td>
+            <td>01/03/2016</td>
+            <td>16/08/2016</td>
+            <td>Programador PHP</td>
+			<td>450€</td>
+            <td><a href="admin-contratos-vista.php">Ver</a></td>
+        </tr>
+        <tr>
+            <td>0051101</td>
+            <td>Pedro Ramirez Garcia</td>
+            <td>31/3/2016</td>
+            <td>31/12/2016</td>
+            <td>Administrador de Sistemas</td>
+			<td>400€</td>
+            <td><a href="admin-contratos-vista.php">Ver</a></td>
+        </tr>
+        </table>
+        </div>
+		<div id="tabla-historial" class="table-container">
+        <div class="table-header"> Contratos finalizados </div>
+        <table class="admin-table">
+        <tr>
+            <th>ID Contrato</th>
+            <th>Estudiante</th>
+            <th>Fecha Inicio</th>
+            <th>Finalización</th>
+            <th>Puesto</th>
+            <th>Sueldo</th>
+			<th>Ver</th>
+        </tr>
+        <tr>
+            <td>0051459</td>
+            <td>Antonio Perez Sanchez</td>
+            <td>12/03/2015</td>
+            <td>30/07/2015</td>
+            <td>Tecnico telecomunicaciones</td>
+			<td>400€</td>
+            <td><a href="admin-contratos-vista.php">Ver</a></td>
+        </tr>
+        <tr>
+        <tr>
+            <td>0057569</td>
+            <td>Jesus Gil y Gil</td>
+            <td>12/03/2015</td>
+            <td>30/07/2015</td>
+            <td>Administrador BBDD</td>
+			<td>400€</td>
+            <td><a href="admin-contratos-vista.php">Ver</a></td>
+        </tr>
+		<tr>
+            <td>0053252</td>
+            <td>Florentino Pérez</td>
+            <td>12/03/2014</td>
+            <td>30/07/2014</td>
+            <td>Programador C++</td>
+			<td>400€</td>
+            <td><a href="admin-contratos-vista.php">Ver</a></td>
+        </tr>
+        <tr>
+            <td>0094059</td>
+            <td>Santiago Bernabeú Gerard</td>
+            <td>12/03/2014</td>
+            <td>30/07/2014</td>
+            <td>Gestor de la web de la empresa</td>
+			<td>400€</td>
+            <td><a href="admin-contratos-vista.php">Ver</a></td>
+        </tr>
+        </table>
+        </div>
 	</div>
   <?php include ('empresa-footer.html'); ?>
   </div>
--- pr2/empresa-solicitudes.php
+++ pr2/empresa-solicitudes.php
@@ -6,7 +6,47 @@
 	<?php include ('empresa-menu.html'); ?>
 	<?php include ('empresa-titlebar.html'); ?>
 	<div id="content-empresa-solicitudes" class="content">
-		
+		 <div id="tabla-ofertas" class="table-container">
+            <div class="table-header"> Solicitudes </div>
+             <table class="admin-table">
+                <tr>
+                    <th>Id Oferta</th>
+                    <th>Puesto</th>
+                    <th>Estudiante</th>
+                    <th>Acción</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Especialista IT</td>
+                    <td><a href="estudiante-perfil.php">Alejandro Martin</a> </td>
+                    <td><a href="">Aceptar / Rechazar</a></td>
+                </tr>
+				<tr>
+                    <td>1</td>
+                    <td>Especialista IT</td>
+                    <td><a href="estudiante-perfil.php">Antonio Recio</a> </td>
+                    <td><a href="">Aceptar / Rechazar</a></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Analista de datos</td>
+                    <td><a href="estudiante-perfil.php">Enrique Pastor</a></td>
+                    <td><a href="">Aceptar / Rechazar</a></td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Analista de datos</td>
+                    <td><a href="estudiante-perfil.php">Juan Cuesta</a></td>
+                    <td><a href="">Aceptar / Rechazar</a></td>
+                </tr>				
+                <tr>
+                    <td>2</td>
+                    <td>Analista de datos</td>
+                    <td><a href="estudiante-perfil.php">Amador Rivas</a></td>
+                    <td><a href="">Aceptar / Rechazar</a></td>
+                </tr>
+            </table>
+        </div>
 	</div>
   <?php include ('empresa-footer.html'); ?>
   </div>
