8c448c28e24a277a2da3c20ac5c5b0c4a3f12797
hriesco <hectorriesco@hotmail.com>
2016-05-13 01:25:06 +0200

    Añadidos widgets demas portales

--- pr3/includes/PortalEstudiante.php
+++ pr3/includes/PortalEstudiante.php
@@ -46,10 +46,90 @@ EOF;
      * Función que genera el contenido de la página principal del portal.
      * El resto de contenido debe generarse por medio de peticiones AJAX.
      */
-    public function generaDashboard()
+ public function generaDashboard()
     {
-        // TODO: Implement generaDashboard() method.
+       $widgets="";
+       $buscador = <<<EOF
+       <div class="dashboard-content">
+
+           <!-- INI Contenedor busqueda dashboard -->
+           <div class="btn-search">
+               <a class="icon-search" href="#">
+                   <i class="fa fa-search fa-2x" style="color:#444;"></i>
+               </a>
+               <div class="txt-search">
+                   <form method="post" action="#">
+                       <input class="txt-search" type="text" placeholder="Buscador de estudiantes / empresas">
+                   </form>
+               </div>
+           </div>
+           <!-- FIN Contenedor busqueda dashboard -->
+
+           <!-- INI Contenedor Widgets superior -->
+           <div class="widget-content">
+EOF;
+
+        //TIPOS DE ICONOS PARA LOS WIDGETS: envelope-o,check-circle,caret-square-o-down,commenting-o (VER FONTAWESOME ICONS)
+
+        /*Generar contenido widget Ofertas */
+        $widgets .= "<!-- INI Widget Ofertas activos -->";
+        $ofertas = OfertaDAO::cargaOfertasNoClasificadas(null,null);
+        $listaOfertas = array();
+        foreach ( $ofertas as $oferta) {
+            $titleItem = $oferta->getEmpresa();
+            $subtitleItem = $oferta->getPuesto();
+            $dias = $oferta->getDiasDesdeCreacion();
+            if ($dias == 0) {$description = "Hoy";} else if ($dias == 1) {$description = "Ayer";} else {$description ="Hace " . $dias . " dias";};
+            $item = array($titleItem,$subtitleItem,$description);
+            array_push($listaOfertas,$item);
+        }
+        $widgets .= parent::generarWidget("Nuevas ofertas", $listaOfertas,"envelope-o","blue");
+        $widgets .= "<!-- FIN Widget Ofertas activos -->";
+
+        /*Generar contenido widget Contratos */
+        $widgets .= "<!-- INI Widget Contratos activos -->";
+        //TODO:Implementar Contrato model & ContratoDAO
+        //$contratos = ContratoDAO::cargaTodosContratosActivos();
+        $contratos = array();
+        $listaContratos = array();
+        foreach ( $contratos as $contrato) {
+
+        }
+        $widgets .= parent::generarWidget("Contratos", $listaContratos,"check-circle","green");
+        $widgets .= "<!-- FIN Widget Contratos activos -->\n<!-- FIN Contenedor widgets superior -->";
+
+        $widgets .= "<!-- INI Contenedor widgets inferior -->\n<!-- INI Contenedor widgets inferior -->";
+
+        /*Generar contenido widget Demandas */
+        $widgets .= "<!-- INI Widget Nuevas demandas -->";
+        //TODO:Implementar Demanda model & DemandaDAO
+        //$demandas = DemandaDAO::cargaTodasDemandas();
+        $demandas = array();
+        $listaDemandas = array();
+        foreach ( $demandas as $demanda) {
+
+        }
+        $widgets .= parent::generarWidget("Demandas", $listaContratos," fa-caret-square-o-down","#FF800D");
+        $widgets .= "<!-- FIN Widget Nuevas demandas -->";
+
+        /*Generar contenido widget Dudas y sugerencias */
+        $widgets .= "<!-- INI Widget Dudas y sugerencias -->";
+        //TODO:Implementar Sugerencia model & SugerenciaDAO
+        //$sugerencias = SugerenciaDAO::cargaTodasSugerencias();
+        $sugerencias = array();
+        $listaSugerencias = array();
+        foreach ( $sugerencias as $sugerencia) {
+
+        }
+        $widgets .= parent::generarWidget("Dudas y sugerencias", $listaContratos," fa-commenting-o","#B9264F");
+        $widgets .= "<!-- FIN Widget Nuevas demandas -->";
+
+        $content = $buscador . $widgets;
+        $content .= "</div>";
+        $content .= "</div>";
+       return $content;
     }
+    
 
     public function generaTitlebar()
     {
