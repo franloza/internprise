d0e9fa2484b9f79a60e3f0d5ce9ba58f324d0b12
Whitenorris <hmalagon@ucm.es>
2016-06-02 22:16:12 +0200

    Todos los portales TERMINADOS

--- includes/FormularioSettings.php
+++ includes/FormularioSettings.php
@@ -104,7 +104,8 @@ EOF;
         $mChecked = ($sexo == 'Mujer') ? "checked" : "";
 
         $content = <<<EOF
-       <legend>Cambios en el perfil: </legend>
+       <legend id= "formSettings">Cambios en el perfil: </legend>
+        <div id = "formSettingsLeft">
         <p><label>Email:</label> <input type="text" name="email" value="$email" disabled/><br /></p>
         <p><label>Password:</label> <input type="password" name="password" value="$password"/><br /></p>
         <p><label>DNI:</label> <input type="text" name="dni" value="$dni"/></p>
@@ -115,8 +116,10 @@ EOF;
         <p><label>Sexo:</label> 
              <input type="radio" name="sexo" value="Hombre" $hChecked >Hombre 
              <input type="radio" name="sexo" value="Mujer" $mChecked > Mujer <br></p>
-        <p><label>Nacionalidad:</label> <input type="text" name="nacionalidad" value="$nacionalidad"/><br /></p>
         <p><label>Direccion:</label> <input type="text" size="50" name="direccion" value="$direccion"/></p>
+        </div>
+        <div id = "formSettingsRight">
+        <p><label>Nacionalidad:</label> <input type="text" name="nacionalidad" value="$nacionalidad"/><br /></p>
         <p><label>Fecha Nacimiento:</label> <input type="date" name="fecha_nacimiento" value="$fecha_nacimiento"/><br /></p>
         <p><label>Codigo Postal:</label> <input type="text" name="cp" value="TODO:Incluir en BBDD"/><br /></p>
         <p><label>Localidad:</label> <input type="text" name="localidad" value="$localidad"/></p>
@@ -124,7 +127,10 @@ EOF;
         <p><label>Pais:</label> <input type="text" name="pais" value="$pais"/></p>
         <p><label>Web:</label> <input type="text" name="web" value="$web"/><br /></p>
         <p><label>Telefono:</label> <input type="text" name="telefono" value="$telefono"/></p>
-        <button type="submit">Confirmar cambios</button>
+        </div>
+        <legend id= "formSettingsSubmit">
+        <button class="btn btn-default" type="submit" id="formSettingsBotton">Confirmar cambios</button>
+        </legend>
 EOF;
         return $content;
     }
@@ -147,19 +153,25 @@ EOF;
         $cp = $user->getCp();
 
         $content = <<<EOF
-       <legend>Cambios en el perfil: </legend>
+       <legend id= "formSettings">Cambios en el perfil: </legend>
+        <div id = "formSettingsLeft">
       <p><label>eMail:</label> <input type="text" name="email" value="$email" disabled/></p>
       <p><label>Password:</label> <input type="password" name="password" value="$password"/><br /></p>
       <p><label>CIF:</label> <input type="text" name="cif" value="$cif"/></p>
       <p><label>Razon Social:</label> <input type="text" name="razonSocial" value="$razonSocial"/><br /></p>
       <p><label>Direccion:</label> <input type="text" size="50" name="direccion" value="$direccion"/></p>
+      </div>
+      <div id = "formSettingsRight">
       <p><label>Codigo Postal:</label> <input type="text" name="cp" value="$cp"/><br /></p>
       <p><label>Localidad:</label> <input type="text" name="localidad" value="$localidad"/></p>
       <p><label>Provincia:</label> <input type="text" name="provincia" value="$provincia"/><br /></p>
       <p><label>Pais:</label> <input type="text" name="pais" value="$pais"/></p>
       <p><label>Web:</label> <input type="text" name="web" value="$web"/><br /></p>
       <p><label>Telefono:</label> <input type="text" name="telefono" value="$telefono"/></p>
-        <button type="submit">Confirmar cambios</button>
+      </div>
+        <legend id= "formSettingsSubmit">
+        <button class="btn btn-default" type="submit" id="formSettingsBotton">Confirmar cambios</button>
+        </legend>
 EOF;
         return $content;
     }
