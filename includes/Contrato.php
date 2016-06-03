faa666387f1a5598eca0f248a566240d6d201b43
Alejandro Martin <alejandromartin@rocky.rediris.es>
2016-06-03 14:12:45 +0200

    creacion contrato, contratoDao , todavia queda cosas, luego sigo

---
+++ includes/Contrato.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace es\ucm\aw\internprise;
+
+
+class Contrato
+{
+    /**
+     * Atributos
+     */
+    private $id_contrato;
+    private $empresa;
+    private $estudiante;
+    private $puesto;
+    private $fecha_inicio;
+    private $fecha_fin;
+    private $horas;
+    private $salario;
+    private $descripcion;
+    private $finalizado; //si o no(en curso)
+
+    /**
+     * Contrato constructor.
+     * @param $id_contrato
+     * @param $empresa
+     * @param $estudiante
+     */
+    public function __construct($id_contrato, $empresa, $estudiante)
+    {
+        $this->$id_contrato = $id_contrato;
+        $this->$empresa = $empresa;
+        $this->$estudiante = $estudiante;
+    }
+
+    /**
+     * Getters & Setters
+     */
+
+
+    /**
+     * @return mixed
+     */
+    public function getIdContrato()
+    {
+        return $this->id_contrato;
+    }
+
+    /**
+     * @param mixed $id_contrato
+     */
+    public function setIdContrato($id_contrato)
+    {
+        $this->id_contrato = $id_contrato;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmpresa()
+    {
+        return $this->empresa;
+    }
+
+    /**
+     * @param mixed $empresa
+     */
+    public function setEmpresa($empresa)
+    {
+        $this->empresa = $empresa;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEstudiante()
+    {
+        return $this->estudiante;
+    }
+
+    /**
+     * @param mixed $estudiante
+     */
+    public function setEstudiante($estudiante)
+    {
+        $this->estudiante = $estudiante;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPuesto()
+    {
+        return $this->puesto;
+    }
+
+    /**
+     * @param mixed $puesto
+     */
+    public function setPuesto($puesto)
+    {
+        $this->puesto = $puesto;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFechaInicio()
+    {
+        return $this->fecha_inicio;
+    }
+
+    /**
+     * @param mixed $fecha_inicio
+     */
+    public function setFechaInicio($fecha_inicio)
+    {
+        $this->fecha_inicio = $fecha_inicio;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFechaFin()
+    {
+        return $this->fecha_fin;
+    }
+
+    /**
+     * @param mixed $fecha_fin
+     */
+    public function setFechaFin($fecha_fin)
+    {
+        $this->fecha_fin = $fecha_fin;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHoras()
+    {
+        return $this->horas;
+    }
+
+    /**
+     * @param mixed $horas
+     */
+    public function setHoras($horas)
+    {
+        $this->horas = $horas;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSalario()
+    {
+        return $this->salario;
+    }
+
+    /**
+     * @param mixed $salario
+     */
+    public function setSalario($salario)
+    {
+        $this->salario = $salario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * @param mixed $descripcion
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    /**
+     * @param mixed $estado
+     */
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+    }
+
+
+}
